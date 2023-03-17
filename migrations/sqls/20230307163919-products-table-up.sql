479c2984e31c761b56657860a26add857a372438
AndrewGithinji <andrewgithinji@hotmail.com>
2023-03-17 21:46:33 +0300

    Deleted Migration Files

--- migrations/sqls/20230307163919-products-table-up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name varchar(255) NOT NULL,
-    price float NOT NULL,
-    category varchar(100) NOT NULL
-);
--- migrations/sqls/20230307164525-users-table-up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    first_name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL
-);
